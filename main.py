54457c7678b03d948a39f837be3b834e98f414e5
king398 <mithilsalunkhe@outlook.com>
2021-10-05 10:47:52 +0530

    sa

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
-
+import numpy as np
 train = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\train.csv")
 sumofvalues = []
 pawpularity = []
-for i in range(10):
+for i in range(9000):
 	x1 = train.loc[i, :].values.tolist()
 	x1.pop(0)
 	pawpularity.append(int(x1[12]))
 	x1.pop(12)
 	sumofvalues.append(sum(list(x1)))
-print(sumofvalues)
-print(pawpularity)
+print(np.mean(sumofvalues))
+print(np.average(pawpularity))
