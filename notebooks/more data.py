8f0182099910bf5f5ef8f728c2edc102ee4c314c
king398 <mithilsalunkhe@outlook.com>
2021-10-19 11:18:46 +0530

    sa

--- notebooks/more data.py
+++ notebooks/more data.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\notebooks\more data.py")
+df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\train_1.csv")
+print(df["AdoptionSpeed"].values())
