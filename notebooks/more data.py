4297f3d5c17c5752de05b6bab397929fd86fe2df
king398 <mithilsalunkhe@outlook.com>
2021-10-20 09:09:49 +0530

    sa

--- notebooks/more data.py
+++ notebooks/more data.py
@@ -10,19 +10,8 @@ df["AdoptionSpeed"] = 25 * (4 - df["AdoptionSpeed"].values)
 pawpularity = pd.concat([df1["Pawpularity"], df["AdoptionSpeed"]])
 ids = []
 labels = []
-
-
-def x():
-	for i, x in zip(df["PetID"].values, df["AdoptionSpeed"].values):
-		all_names = glob.glob(rf"F:\Pycharm_projects\PetFinder\data\train_old/{i}-*.jpg")
-		for y in all_names:
-			ids.append(y)
-			labels.append(x)
-
-
-from multiprocessing import Process
-
-if __name__ == '__main__':
-	p = Process(target=x, args=())
-	p.start()
-	p.join()
+for i, x in tqdm(zip(df["PetID"].values, df["AdoptionSpeed"].values)):
+	all_names = glob.glob(rf"F:\Pycharm_projects\PetFinder\data\train_old/{i}-*.jpg")
+	for y in all_names:
+		ids.append(y)
+		labels.append(x)
