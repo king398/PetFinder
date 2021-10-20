833421bd168dc8022017501621b84c77680881be
king398 <mithilsalunkhe@outlook.com>
2021-10-20 10:19:45 +0530

    sa

--- notebooks/more data.py
+++ notebooks/more data.py
@@ -3,22 +3,13 @@ import os
 import glob
 from tqdm import tqdm
 import tensorflow as tf
+import numpy as np
 
 df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\train_1.csv")
 df1 = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\train.csv")
 df["AdoptionSpeed"] = 25 * (4 - df["AdoptionSpeed"].values)
-
-pawpularity = pd.concat([df1["Pawpularity"], df["AdoptionSpeed"]])
-ids = []
-labels = []
-
-for i, x in tqdm(zip(df["PetID"].values, df["AdoptionSpeed"].values)):
-	all_names = glob.glob(rf"F:\Pycharm_projects\PetFinder\data\train_old/{i}-*.jpg")
-
-	for y in all_names:
-		ids.append(y)
-		labels.append(x)
-import numpy as np
-
-np.save("F:\Pycharm_projects\PetFinder\data/idold.npy", np.array(ids))
-np.save("F:\Pycharm_projects\PetFinder\data/idold.npy", np.array(labels))
+ids = np.load("F:\Pycharm_projects\PetFinder\data\idold.npy")
+labels = np.load("F:\Pycharm_projects\PetFinder\data\labels.npy")
+pawpularity = np.concatenate([df1["Pawpularity"].values, labels])
+for i in ids:
+	i.spilt(r"\\")
