2d6c708ec3b05cbb23be0c25fe9eea14d1a6f1de
king398 <mithilsalunkhe@outlook.com>
2021-10-31 16:03:15 +0530

    sa

--- notebooks/Spilt.py
+++ notebooks/Spilt.py
@@ -1,8 +1,3 @@
 import pandas as pd
 
-df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\cat_class.csv")
-df = df[df.is_cat != 0]
-df.to_csv(r"F:\Pycharm_projects\PetFinder\data\catonly.csv")
-df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\cat_class.csv")
-df = df[df.is_cat != 1]
-df.to_csv(r"F:\Pycharm_projects\PetFinder\data\dogonly.csv")
+df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\catonly.csv")
