db6e11e097f174b3bfb049665491825465405ee5
king398 <mithilsalunkhe@outlook.com>
2021-10-31 20:46:38 +0530

    sa

--- notebooks/Cats and dogs.py
+++ notebooks/Cats and dogs.py
@@ -1,7 +1,9 @@
-import timm
 import torch
+import torchvision
+import timm
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
 images = torch.rand(1, 3, 384, 384)
 
@@ -29,3 +31,25 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Model()
 model.to(device)
 
+import cv2
+
+
+class CuteDataset(Dataset):
+	def __init__(self, image_path):
+		self.image_path = image_path
+
+	def __len__(self):
+		return len(self.image_path)
+
+	def __getitem__(self, idx):
+		image_file = self.image_path[idx]
+		image = cv2.imread(image_file)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		print(image_file.spilt("//"))
+		image = torch.tensor(image)
+		return image
+
+
+train = CuteDataset(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train\cat.0.jpg")
+for i in train:
+	print(train)
