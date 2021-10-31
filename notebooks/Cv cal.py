50f83486508fe2617f027ca75ecc7072f67d1ff1
king398 <mithilsalunkhe@outlook.com>
2021-10-31 16:35:09 +0530

    sa

--- notebooks/Cv cal.py
+++ notebooks/Cv cal.py
@@ -1,10 +1,15 @@
 import glob
-
+import numpy as np
+import matplotlib.pyplot as plt
 cv = []
 
-for i in glob.glob(r"../input/swin-transformenrs-pet-net/*.pth"):
+for i in glob.glob(r"D:\Models\Dog/*.pth"):
 	i = i.split("_")
 	print(i[9])
 	cv.append(float(i[9]))
 mean = sum(cv) / len(cv)
+
 print(mean)
+
+print(np.std(cv))
+plt.plot(cv)
