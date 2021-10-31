bc5ce444340073ef44c5f3ba79c83cd9870fe5a9
king398 <mithilsalunkhe@outlook.com>
2021-10-31 20:32:59 +0530

    sa

--- notebooks/Cats and dogs.py
+++ notebooks/Cats and dogs.py
@@ -1,105 +1,31 @@
-import torch
-from torch import nn
-from torch import optim
-import torch.nn.functional as F
-from torchvision import datasets, transforms, models
-
-data_dir = r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs"
 import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(384),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406],
-                                                            [0.229, 0.224, 0.225])])
+images = torch.rand(1, 3, 384, 384)
 
-test_transforms = transforms.Compose([transforms.Resize(384),
-                                      transforms.CenterCrop(384),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])])
 
-# Pass transforms in here, then run the next cell to see how the transforms look
-train_data = datasets.ImageFolder('F:\Pycharm_projects\PetFinder\data\Cats And Dogs\PetImages', transform=train_transforms)
+class Model(nn.Module):
+	def __init__(self):
+		super(Model, self).__init__()
+		self.model = timm.create_model("swin_large_patch4_window12_384_in22k", pretrained=True)
+		n_features = self.model.head.in_features
+		self.model.head = nn.Linear(n_features, 128)
+		self.fc = nn.Sequential(
+			nn.Linear(128, 64),
+			nn.ReLU(),
+			nn.Linear(64, 1),
+		)
 
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=8, shuffle=True)
-model = timm.create_model("swin_large_patch4_window12_384_in22k", pretrained=True, num_classes=1)
-model
-# Freeze parameters so we don't backprop through them
-for param in model.parameters():
-    param.requires_grad = False
+	def forward(self, x):
+		x = self.model(x)
+		print(x)
+		x = self.fc(x)
+		return x
 
-from collections import OrderedDict
-
-classifier = nn.Sequential(OrderedDict([
-    ('fc1', nn.Linear(1024, 512)),
-    ('relu1', nn.ReLU()),
-    ('fc2', nn.Linear(512, 256)),
-    ('relu2', nn.ReLU()),
-    ('fc3', nn.Linear(256, 2)),
-    ('output', nn.LogSigmoid())
-]))
-
-model.classifier = classifier
-# Use GPU if it's available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-# Freeze parameters so we don't backprop through them
-for param in model.parameters():
-    param.requires_grad = False
-
-model.classifier = nn.Sequential(nn.Linear(1024, 512),
-                                 nn.ReLU(),
-                                 nn.Dropout(0.2),
-                                 nn.Linear(512, 256),
-                                 nn.ReLU(),
-                                 nn.Dropout(0.1),
-                                 nn.Linear(256, 2),
-                                 nn.LogSigmoid())
-
-criterion = nn.NLLLoss()
-
-# Only train the classifier parameters, feature parameters are frozen
-optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
-
+model = Model()
 model.to(device)
-
-traininglosses = []
-testinglosses = []
-testaccuracy = []
-totalsteps = []
-epochs = 1
-steps = 0
-running_loss = 0
-print_every = 5
-from tqdm import tqdm
-for epoch in tqdm(range(epochs)):
-    for inputs, labels in trainloader:
-        steps += 1
-        # Move input and label tensors to the default device
-        inputs, labels = inputs.to(device), labels.to(device)
-
-        optimizer.zero_grad()
-
-        logps = model.forward(inputs)
-        loss = criterion(logps, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-
-        if steps % print_every == 0:
-            test_loss = 0
-            accuracy = 0
-
-
-            traininglosses.append(running_loss / print_every)
-
-            totalsteps.append(steps)
-            print(f"Device {device}.."
-                  f"Epoch {epoch + 1}/{epochs}.. "
-                  f"Step {steps}.. "
-                  f"Train loss: {running_loss / print_every:.3f}.. ")
-            running_loss = 0
+images = images.to(device)
+print(model(images))
