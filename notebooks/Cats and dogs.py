5894603e9903994501a57722b6c62ae3deb65c50
king398 <mithilsalunkhe@outlook.com>
2021-11-01 11:49:27 +0530

    sa

--- notebooks/Cats and dogs.py
+++ notebooks/Cats and dogs.py
@@ -34,22 +34,42 @@ model.to(device)
 import cv2
 
 
+def get_valid_transforms(DIM=384):
+	return albumentations.Compose(
+		[
+			albumentations.Resize(DIM, DIM),
+			albumentations.Normalize(
+				mean=[0.485, 0.456, 0.406],
+				std=[0.229, 0.224, 0.225],
+			),
+			ToTensorV2(p=1.0)
+		]
+	)
+
+
 class CuteDataset(Dataset):
-	def __init__(self, image_path):
+	def __init__(self, image_path, transform=get_valid_transforms(384)):
 		self.image_path = image_path
+		self.transform = transform
 
 	def __len__(self):
 		return len(self.image_path)
 
 	def __getitem__(self, idx):
+		print(idx)
 		image_file = self.image_path[idx]
 		image = cv2.imread(image_file)
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		print(image_file.spilt("//"))
-		image = torch.tensor(image)
+		if self.transform is not None:
+			image = self.transform(image=image)['image']
+		image_file = image_file.split("\\")
+		print(image_file)
 		return image
 
 
-train = CuteDataset(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train\cat.0.jpg")
+import albumentations
+from albumentations.pytorch.transforms import ToTensorV2
+
+train = CuteDataset(image_path=[r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train\cat.0.jpg"])
 for i in train:
-	print(train)
+	print(i)
