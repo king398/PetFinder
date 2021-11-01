c198f6f95a67f1fdbcc218973d42133f14eab4cf
king398 <mithilsalunkhe@outlook.com>
2021-11-01 20:35:05 +0530

    sa

--- notebooks/Aug trying.py
+++ notebooks/Aug trying.py
@@ -2,5 +2,33 @@ import albumentations
 import PIL
 import numpy as np
 import matplotlib.pyplot as plt
-image = PIL.Image.open(r"F:\Pycharm_projects\PetFinder\data\train\0a0da090aa9f0342444a7df4dc250c66.jpg")
-image = np.array(image)
+import glob
+import random
+
+
+def get_train_transforms(DIM=1024):
+	return albumentations.Compose(
+		[
+			albumentations.RandomResizedCrop(DIM, DIM),
+			albumentations.Normalize(
+				mean=[0.485, 0.456, 0.406],
+				std=[0.229, 0.224, 0.225],
+			),
+
+		]
+	)
+
+
+import cv2
+
+paths = glob.glob(r"../input/petfinder-pawpularity-score/train/*.jpg")
+for i in range(10):
+	image_random = random.choice(paths)
+	srcBGR = cv2.imread(image_random)
+	image = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
+	image = np.array(image)
+
+	transform = get_train_transforms()
+	image = transform(image=image)['image']
+	plt.imshow(image)
+	plt.show()
