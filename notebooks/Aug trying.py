10d5d28e4e21fc70ca1e9dc28bdbaad6feb42f2c
king398 <mithilsalunkhe@outlook.com>
2021-11-01 19:57:18 +0530

    sa

--- notebooks/Aug trying.py
+++ notebooks/Aug trying.py
@@ -1,3 +1,6 @@
 import albumentations
 import PIL
-image = PIL.Image.open()
+import numpy as np
+import matplotlib.pyplot as plt
+image = PIL.Image.open(r"F:\Pycharm_projects\PetFinder\data\train\0a0da090aa9f0342444a7df4dc250c66.jpg")
+image = np.array(image)
