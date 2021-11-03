38b93ec8696fcb3e82c1237c087d6a6edd94e7d2
king398 <mithilsalunkhe@outlook.com>
2021-11-03 09:46:18 +0530

    sa

--- notebooks/Random selection.py
+++ notebooks/Random selection.py
@@ -2,8 +2,17 @@ import glob
 from shutil import copyfile
 import os
 import random
+from tqdm import tqdm
+import shutil
 
 x = os.listdir(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train")
 print(os.path.join(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train", x[0]))
 selected = random.choices(x, k=10000)
 print(len(selected))
+for i in tqdm(selected):
+	try:
+		path = os.path.join(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train", i)
+		des = os.path.join(r"F:\Pycharm_projects\PetFinder\data\pseudo", i)
+		shutil.move(path, des)
+	except:
+		pass
