72aa7f0190fc2f31adee8cae9f66601067693180
king398 <mithilsalunkhe@outlook.com>
2021-11-03 12:09:12 +0530

    sa

--- notebooks/transformers-classifier-method-starter-train.py
+++ notebooks/transformers-classifier-method-starter-train.py
@@ -317,6 +317,26 @@ class CuteDataset(Dataset):
 		return image, dense, label
 
 
+class CuteDatasetPseudo(Dataset):
+	def __init__(self, images_filepaths, transform=None):
+		self.images_filepaths = images_filepaths
+
+		self.transform = transform
+
+	def __len__(self):
+		return len(self.images_filepaths)
+
+	def __getitem__(self, idx):
+		image_filepath = self.images_filepaths[idx]
+		image = cv2.imread(image_filepath)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+		if self.transform is not None:
+			image = self.transform(image=image)['image']
+
+		return image
+
+
 # ## 1. Visualize Some Examples
 
 # In[ ]:
@@ -488,10 +508,12 @@ def alpha_weight(step):
 		return ((step - T1) / (T2 - T1)) * af
 
 
-def train_fn(train_loader, model, criterion, optimizer, epoch, params, scheduler=None):
+def train_fn(train_loader, model, criterion, optimizer, epoch, params, pseudo_loader, scheduler=None
+             ):
 	metric_monitor = MetricMonitor()
 	model.train()
 	stream = tqdm(train_loader)
+	stream_pseudo = tqdm(pseudo_loader)
 
 	for i, (images, dense, target) in enumerate(stream, start=1):
 		if params['mixup']:
