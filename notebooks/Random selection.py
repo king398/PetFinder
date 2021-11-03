742cb19813ad1b858f3b9dedab4e86fa9349562f
king398 <mithilsalunkhe@outlook.com>
2021-11-03 09:35:12 +0530

    sa

--- notebooks/Random selection.py
+++ notebooks/Random selection.py
@@ -1,6 +1,9 @@
 import glob
 from shutil import copyfile
 import os
-	
-x = os.listdir(r"F:\Pycharm_projects\PetFinder\data\train")
-print(x[0])
+import random
+
+x = os.listdir(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train")
+print(os.path.join(r"F:\Pycharm_projects\PetFinder\data\Cats And Dogs\train", x[0]))
+selected = random.choices(x, k=10000)
+print(len(selected))
