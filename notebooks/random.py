333e3cbd1585828af07adc122042568ab1bfb5b2
king398 <mithilsalunkhe@outlook.com>
2021-11-05 10:41:56 +0530

    sa

--- notebooks/random.py
+++ notebooks/random.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.random.randn(15, 15, 3)
-plt.imshow(x)
+
+images = np.empty(shape=(30, 30, 3))
+for i in range(10):
+	x = np.random.randn(30, 30, 3)
+	images = np.hstack((x, images))
+images = np.array(images)
+plt.imshow(images)
 plt.show()
--- notebooks/transformers-classifier-method-starter-train.py
+++ notebooks/transformers-classifier-method-starter-train.py
@@ -466,6 +466,114 @@ def get_scheduler(optimizer, scheduler_params=params):
 # The complete list of Pytorch pre-trained image models through timm can be found [here](https://rwightman.github.io/pytorch-image-models/)
 
 # In[ ]:
+from timm.models.layers import get_act_layer
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicConv(nn.Module):
+	def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True,
+	             bn=True, bias=False,
+	             act_layer=nn.ReLU):
+		super(BasicConv, self).__init__()
+		self.out_channels = out_planes
+		self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
+		                      dilation=dilation, groups=groups, bias=bias)
+		self.bn = nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
+		self.relu = act_layer if relu else None
+
+	def forward(self, x):
+		x = self.conv(x)
+		if self.bn is not None:
+			x = self.bn(x)
+		if self.relu is not None:
+			x = self.relu(x, inplace=True)
+		return x
+
+
+class ChannelPool(nn.Module):
+	def forward(self, x):
+		return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+
+class SpatialGate(nn.Module):
+	def __init__(self, act_layer=nn.ReLU, kernel_size=7):
+		super(SpatialGate, self).__init__()
+		self.compress = ChannelPool()
+		self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False,
+		                         act_layer=act_layer)
+
+	def forward(self, x):
+		x_compress = self.compress(x)
+		x_out = self.spatial(x_compress)
+		scale = torch.sigmoid_(x_out)
+		return x * scale
+
+
+class TripletAttention(nn.Module):
+	def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False,
+	             act_layer=nn.ReLU, kernel_size=7):
+		super(TripletAttention, self).__init__()
+		self.ChannelGateH = SpatialGate(act_layer=act_layer, kernel_size=kernel_size)
+		self.ChannelGateW = SpatialGate(act_layer=act_layer, kernel_size=kernel_size)
+		self.no_spatial = no_spatial
+		if not no_spatial:
+			self.SpatialGate = SpatialGate(kernel_size=kernel_size)
+
+	def forward(self, x):
+		x_perm1 = x.permute(0, 2, 1, 3).contiguous()
+		x_out1 = self.ChannelGateH(x_perm1)
+		x_out11 = x_out1.permute(0, 2, 1, 3).contiguous()
+		x_perm2 = x.permute(0, 3, 2, 1).contiguous()
+		x_out2 = self.ChannelGateW(x_perm2)
+		x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
+		if not self.no_spatial:
+			x_out = self.SpatialGate(x)
+			x_out = (1 / 3) * (x_out + x_out11 + x_out21)
+		else:
+			x_out = (1 / 2) * (x_out11 + x_out21)
+		return x_out
+
+
+class BlockAttentionModel(nn.Module):
+	def __init__(
+			self,
+			backbone: nn.Module,
+			n_features: int,
+	):
+		"""Initialize"""
+		super(BlockAttentionModel, self).__init__()
+		self.backbone = backbone
+		self.n_features = n_features
+		self.drop_rate = 0.2
+		act_layer = get_act_layer("swish")
+
+		self.attention = TripletAttention(self.n_features,
+		                                  act_layer=act_layer,
+		                                  kernel_size=13)
+
+		self.head = nn.Linear(self.n_features, 128)
+
+	def _init_params(self):
+		nn.init.xavier_normal_(self.fc.weight)
+		if type(self.fc.bias) == torch.nn.parameter.Parameter:
+			nn.init.constant_(self.fc.bias, 0)
+		nn.init.constant_(self.bn.weight, 1)
+		nn.init.constant_(self.bn.bias, 0)
+
+	def forward(self, x, t=None):
+		"""Forward"""
+		x = self.backbone(x)
+		x = self.attention(x)
+		x = self.global_pool(x)
+		x = x.view(x.size(0), -1)
+		if self.drop_rate:
+			x = F.dropout(x, p=float(self.drop_rate), training=self.training)
+		x = self.head(x)
+		return x
+
 
 class PetNet(nn.Module):
 	def __init__(self, model_1_name=params['model_1'], model_2_name=params['model_2'],
@@ -492,8 +600,13 @@ class PetNet(nn.Module):
 		                                   stride=stride, padding=padding,
 		                                   bias=bias)
 		n_features_2 = self.model_2.classifier.in_features
-		self.model_2.classifier = nn.Linear(n_features_2, 128)
+		act_layer = get_act_layer("swish")
 
+		self.attention = TripletAttention(n_features_2,
+		                                  act_layer=act_layer,
+		                                  kernel_size=13)
+		self.head_conv = nn.Linear(n_features_2, 128)
+		self.model_2.reset_classifier(0, '')
 		self.fc = nn.Sequential(
 			nn.Linear(128 + 128 + num_dense, 64),
 			nn.ReLU(),
@@ -506,11 +619,11 @@ class PetNet(nn.Module):
 		self.hidden_size = 2
 		self.seq_length = 4
 
-
 	def forward(self, image, dense):
 		transformer_embeddings = self.model_1(image)
 		conv_embeddings = self.model_2(image)
-
+		conv_embeddings = self.attention(conv_embeddings)
+		conv_embeddings = self.head_conv(conv_embeddings)
 
 		features = torch.cat([transformer_embeddings, conv_embeddings, dense],
 		                     dim=1)
@@ -518,6 +631,7 @@ class PetNet(nn.Module):
 		x = self.dropout(features)
 		output = self.fc(x)
 		return output
+
 
 # # Train and Validation Functions
 
