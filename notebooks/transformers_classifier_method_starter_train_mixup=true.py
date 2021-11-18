5a0d56dcfcfb04045c2c78ee36e5d09ccf987fdc
king398 <mithilsalunkhe@outlook.com>
2021-11-18 19:15:56 +0530

    sa

--- notebooks/transformers_classifier_method_starter_train_mixup=true.py
+++ notebooks/transformers_classifier_method_starter_train_mixup=true.py
@@ -308,7 +308,7 @@ def mixup_criterion(criterion, pred, y_a, y_b, lam):
 	return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 
-def rand_bbox(size, lamb):
+def rand_bbox1(size, lamb):
 	""" Generate random bounding box
 	Args:
 		- size: [width, breadth] of the bounding box
@@ -348,7 +348,7 @@ def cutmix(image_batch, image_batch_labels, beta=params["mixup_alpha"]):
 	rand_index = np.random.permutation(len(image_batch))
 	target_a = image_batch_labels
 	target_b = image_batch_labels[rand_index]
-	bbx1, bby1, bbx2, bby2 = rand_bbox(image_batch[0].shape, lam)
+	bbx1, bby1, bbx2, bby2 = rand_bbox1(image_batch[0].shape, lam)
 	image_batch_updated = image_batch.clone()
 	image_batch_updated[:, bbx1:bbx2, bby1:bby2, :] = image_batch[rand_index, bbx1:bbx2, bby1:bby2, :]
 
@@ -357,6 +357,130 @@ def cutmix(image_batch, image_batch_labels, beta=params["mixup_alpha"]):
 	label = target_a * lam + target_b * (1. - lam)
 
 	return image_batch_updated, label
+
+
+def rand_bbox(size, lam, center=False, attcen=None):
+	if len(size) == 4:
+		W = size[2]
+		H = size[3]
+	elif len(size) == 3:
+		W = size[1]
+		H = size[2]
+	elif len(size) == 2:
+		W = size[0]
+		H = size[1]
+	else:
+		raise Exception
+
+	cut_rat = np.sqrt(1. - lam)
+	cut_w = np.int(W * cut_rat)
+	cut_h = np.int(H * cut_rat)
+
+	if attcen is None:
+		# uniform
+		cx = 0
+		cy = 0
+		if W > 0 and H > 0:
+			cx = np.random.randint(W)
+			cy = np.random.randint(H)
+		if center:
+			cx = int(W / 2)
+			cy = int(H / 2)
+	else:
+		cx = attcen[0]
+		cy = attcen[1]
+
+	bbx1 = np.clip(cx - cut_w // 2, 0, W)
+	bby1 = np.clip(cy - cut_h // 2, 0, H)
+	bbx2 = np.clip(cx + cut_w // 2, 0, W)
+	bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+	return bbx1, bby1, bbx2, bby2
+
+
+def get_bbox(imgsize=(224, 224), beta=1.0):
+	r = np.random.rand(1)
+	lam = np.random.beta(beta, beta)
+	bbx1, bby1, bbx2, bby2 = rand_bbox(imgsize, lam)
+
+	return [bbx1, bby1, bbx2, bby2]
+
+
+def get_spm(input, target, model):
+	imgsize = (384, 384)
+	bs = input.size(0)
+	with torch.no_grad():
+		output, fms, _ = model(input)
+
+		clsw = model.module.fc
+		weight = clsw.weight.data
+		bias = clsw.bias.data
+		weight = weight.view(weight.size(0), weight.size(1), 1, 1)
+		fms = F.relu(fms)
+		poolfea = F.adaptive_avg_pool2d(fms, (1, 1)).squeeze()
+		clslogit = F.softmax(clsw.forward(poolfea))
+		logitlist = []
+		for i in range(bs):
+			logitlist.append(clslogit[i, target[i]])
+		clslogit = torch.stack(logitlist)
+
+		out = F.conv2d(fms, weight, bias=bias)
+
+		outmaps = []
+		for i in range(bs):
+			evimap = out[i, target[i]]
+			outmaps.append(evimap)
+
+		outmaps = torch.stack(outmaps)
+		if imgsize is not None:
+			outmaps = outmaps.view(outmaps.size(0), 1, outmaps.size(1), outmaps.size(2))
+			outmaps = F.interpolate(outmaps, imgsize, mode='bilinear', align_corners=False)
+
+		outmaps = outmaps.squeeze()
+
+		for i in range(bs):
+			outmaps[i] -= outmaps[i].min()
+			outmaps[i] /= outmaps[i].sum()
+
+	return outmaps, clslogit
+
+
+def snapmix(input, target, model=None):
+	r = np.random.rand(1)
+	lam_a = torch.ones(input.size(0))
+	lam_b = 1 - lam_a
+	target_b = target.clone()
+
+	if r < 1:
+		wfmaps, _ = get_spm(input, target, model)
+		bs = input.size(0)
+		lam = np.random.beta(5, 5)
+		lam1 = np.random.beta(5, 5)
+		rand_index = torch.randperm(bs).cuda()
+		wfmaps_b = wfmaps[rand_index, :, :]
+		target_b = target[rand_index]
+
+		same_label = target == target_b
+		bbx1, bby1, bbx2, bby2 = rand_bbox(input.size(), lam)
+		bbx1_1, bby1_1, bbx2_1, bby2_1 = rand_bbox(input.size(), lam1)
+
+		area = (bby2 - bby1) * (bbx2 - bbx1)
+		area1 = (bby2_1 - bby1_1) * (bbx2_1 - bbx1_1)
+
+		if area1 > 0 and area > 0:
+			ncont = input[rand_index, :, bbx1_1:bbx2_1, bby1_1:bby2_1].clone()
+			ncont = F.interpolate(ncont, size=(bbx2 - bbx1, bby2 - bby1), mode='bilinear', align_corners=True)
+			input[:, :, bbx1:bbx2, bby1:bby2] = ncont
+			lam_a = 1 - wfmaps[:, bbx1:bbx2, bby1:bby2].sum(2).sum(1) / (wfmaps.sum(2).sum(1) + 1e-8)
+			lam_b = wfmaps_b[:, bbx1_1:bbx2_1, bby1_1:bby2_1].sum(2).sum(1) / (wfmaps_b.sum(2).sum(1) + 1e-8)
+			tmp = lam_a.clone()
+			lam_a[same_label] += lam_b[same_label]
+			lam_b[same_label] += tmp[same_label]
+			lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (input.size()[-1] * input.size()[-2]))
+			lam_a[torch.isnan(lam_a)] = lam
+			lam_b[torch.isnan(lam_b)] = 1 - lam
+
+	return input, target, target_b, lam_a.cuda(), lam_b.cuda()
 
 
 """## 3. Valid Augmentations"""
