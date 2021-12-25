0bb9692dbb66204d54322eb862e96006cc3525a9
king398 <mithilsalunkhe@outlook.com>
2021-12-25 09:33:49 +0530

    sa

--- notebooks/oof preds.py
+++ notebooks/oof preds.py
@@ -109,18 +109,6 @@ params = {
 }
 
 
-def get_valid_transforms(DIM=params['im_size']):
-	return albumentations.Compose(
-		[
-			albumentations.Resize(DIM, DIM),
-			albumentations.Normalize(
-				mean=[0.485, 0.456, 0.406],
-				std=[0.229, 0.224, 0.225],
-			),
-			ToTensorV2(p=1.0)
-		]
-	)
-
 
 def get_test_transforms(DIM=params['im_size']):
 	return albumentations.Compose(
@@ -140,7 +128,7 @@ def get_test_Flip_transforms(DIM=params['im_size']):
 		[albumentations.HorizontalFlip(p=0.5),
 
 		 albumentations.VerticalFlip(p=0.5),
-		 albumentations.Resize(DIM, DIM),
+		 albumentations.RandomResizedCrop(DIM, DIM),
 		 albumentations.Normalize(
 			 mean=[0.485, 0.456, 0.406],
 			 std=[0.229, 0.224, 0.225],
@@ -156,7 +144,7 @@ def get_test_Shift_Scale_transforms(DIM=params['im_size']):
 			shift_limit=0.1, scale_limit=0.1, rotate_limit=45, p=0.5
 		),
 
-			albumentations.Resize(DIM, DIM),
+			albumentations.RandomResizedCrop(DIM, DIM),
 			albumentations.Normalize(
 				mean=[0.485, 0.456, 0.406],
 				std=[0.229, 0.224, 0.225],
@@ -169,7 +157,7 @@ def get_test_Shift_Scale_transforms(DIM=params['im_size']):
 def get_test_Rotate_transforms(DIM=params['im_size']):
 	return albumentations.Compose(
 		[albumentations.Rotate(limit=180, p=0.7),
-		 albumentations.Resize(DIM, DIM),
+		 albumentations.RandomResizedCrop(DIM, DIM),
 		 albumentations.Normalize(
 			 mean=[0.485, 0.456, 0.406],
 			 std=[0.229, 0.224, 0.225],
