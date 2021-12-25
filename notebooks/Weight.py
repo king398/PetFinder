01ddf4c16f1e3ca7d439d729027b2c962f39a55d
king398 <mithilsalunkhe@outlook.com>
2021-12-25 17:43:32 +0530

    sa

--- notebooks/Weight.py
+++ notebooks/Weight.py
@@ -5,6 +5,7 @@ from sklearn.utils import compute_class_weight
 import pandas as pd
 import numpy as np
 import torch.nn.functional as F
+
 train_df = pd.read_csv(r"F:\Pycharm_projects\PetFinder\data\train_10folds.csv")
 y_train = train_df['Pawpularity'].values
 
@@ -14,22 +15,23 @@ class_weights = compute_class_weight(
 	y_train)
 batch_size = 8
 nb_classes = 1
-output = torch.tensor([0.99, 0.01, 0.98])  # most underrepresented class
-target = torch.tensor([0.80, 0.3, 0.33])
-class_weights = np.expand_dims(class_weights,1)
+output = torch.tensor([-0.0806])  # most underrepresented class
+target = torch.tensor([0.99])
+class_weights = np.expand_dims(class_weights, 1)
 print(class_weights.shape)
 weight = torch.tensor(class_weights)
 
 criterion = nn.BCEWithLogitsLoss(reduction='none')
 
-
-
-
 loss = criterion(output, target)
-loss = loss * weight
+for i,value in enumerate(loss):
+	weights = int(target[i] * 100)
+	weightss = weight[weights-1]
+	print(weightss)
+	loss[i] = loss[i]*weightss
 loss = loss.mean()
 
 print(f"weighted loss {loss}")  #
 criterion = nn.BCEWithLogitsLoss()
-loss = criterion(output, target)
-print(f" non weighted loss {loss}")
+loss1 = criterion(output, target)
+print(f" non weighted loss {loss1}")
