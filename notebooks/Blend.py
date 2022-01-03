086cc9f3332463bf76ed11292e07ad820e951733
king398 <mithilsalunkhe@outlook.com>
2022-01-03 16:00:42 +0530

    sa

--- notebooks/Blend.py
+++ notebooks/Blend.py
@@ -3,15 +3,13 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
 
-df_swin = pd.read_csv("F:\Pycharm_projects\PetFinder\oof files\swin_large_patch4_window12_384_oof_crop.csv")
+df_swin = pd.read_csv(r"F:\Pycharm_projects\PetFinder\oof files\Fastai\train_with_oof_swin_large_384_beta_0.5.csv")
 df_vit = pd.read_csv(
-	"F:\Pycharm_projects\PetFinder\oof files\swin_large_patch4_window7_224_oof_crop.csv")
-df_swin_22k = pd.read_csv(r"F:\Pycharm_projects\PetFinder\oof files\swin_base_patch4_window12_384_in22k_oof.csv")
+	r"F:\Pycharm_projects\PetFinder\oof files\Fastai\train_with_oof_swin_large_224_beta_0.5.csv")
 # train = df_swin.sample(n=1700, random_state=42).reset_index(drop=True)
-true = df_swin["true"].values
-swin_pred = df_swin["pred"].values
-swin_small_pred = df_vit["pred"].values
-swin_22k_pred = df_swin_22k["pred"].values
+true = df_swin["Pawpularity"].values
+swin_pred = df_swin["oof"].values
+swin_small_pred = df_vit["oof"].values
 score = []
 for ww in np.arange(0, 1.05, 0.05):
 	oof3 = (1 - ww) * swin_pred + ww * swin_small_pred
@@ -21,12 +19,5 @@ for ww in np.arange(0, 1.05, 0.05):
 	score.append(rsme)
 best_w = np.argmin(score) * 0.05
 print(best_w)
-plt.figure(figsize=(20, 5))
-plt.plot(np.arange(21) / 20.0, score, '-o')
-plt.plot([best_w], np.min(score), 'o', color='black', markersize=15)
-plt.title(f'Best Overall CV RSME={np.min(score):.4} with SVR Ensemble Weight={best_w:.2}', size=16)
-plt.ylabel('Overall Ensemble RSME', size=14)
-plt.xlabel('SVR Weight', size=14)
-plt.show()
-print(mean_squared_error(true, swin_pred * 1/3 + swin_small_pred * 1/3 + swin_22k_pred * 1/3, squared=False))
-print(mean_squared_error(true, swin_pred, squared=False))
+
+print(mean_squared_error(true, swin_pred * 0.5 + swin_small_pred * 0.5, squared=False))
--- notebooks/Randomcall.py
+++ notebooks/Randomcall.py
@@ -1,1 +1,22 @@
 import random
+
+
+def first():
+	return 1
+
+
+def second():
+	return 2
+
+
+def third():
+	return 3
+
+
+func_dict = {"first": first(), "second": second(), "third": third()}
+ls = ["first", "second", "third"]
+choice = random.choices(ls, k=1)
+for i in choice:
+	func = func_dict[i]
+	result = func
+	print(result)
